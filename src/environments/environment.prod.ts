4c060323acfd8827319955f79393dcdc3abb5293
Joaquin <joaquin.corugedo@campusfp.es>
2020-03-08 14:04:02 +0100

    Retocando algunos aspectos visuales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDH-YRk2waorSTMIBUaGqgsPTAmTrY8a4s",
+    authDomain: "hito-ionic.firebaseapp.com",
+    databaseURL: "https://hito-ionic.firebaseio.com",
+    projectId: "hito-ionic",
+    storageBucket: "hito-ionic.appspot.com",
+    messagingSenderId: "164312309577",
+    appId: "1:164312309577:web:9440ca54eaf8c9c1018498",
+    measurementId: "G-9S66BPMMHK"
+  }
 };
